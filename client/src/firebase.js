e6648df2f90b411fef7e223f7ebd427c558d1f8f
NITISH PATEL <129555493+nitish110922@users.noreply.github.com>
2024-06-24 18:37:38 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,13 +5,22 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: 'mern-blog-b327f.firebaseapp.com',
+//   projectId: 'mern-blog-b327f',
+//   storageBucket: 'mern-blog-b327f.appspot.com',
+//   messagingSenderId: '699397991367',
+//   appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+// };
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-blog-b327f.firebaseapp.com',
-  projectId: 'mern-blog-b327f',
-  storageBucket: 'mern-blog-b327f.appspot.com',
-  messagingSenderId: '699397991367',
-  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+  apiKey: "AIzaSyDeFxi6G7ZLNiUzDNLrCLsYflDOTtmlEb4",
+  authDomain: "nitishblog-40e18.firebaseapp.com",
+  projectId: "nitishblog-40e18",
+  storageBucket: "nitishblog-40e18.appspot.com",
+  messagingSenderId: "75901240541",
+  appId: "1:75901240541:web:8555fb4dc176bbf8070734"
 };
 
 // Initialize Firebase
